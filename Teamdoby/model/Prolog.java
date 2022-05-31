039c28ea74f65cbab5c5142232cc0ac30dc06e46
smhrd <smhrd@59.0.129.180>
2022-05-31 15:50:01 +0900

    메소드 추가

---
+++ Teamdoby/model/Prolog.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Prolog {
+
+	public static void main(String[] args) {
+		String name = "ㅇㅇ";
+		
+		System.out.println("  ___    _____      _    _                     _              \r\n"
+				+ " / _ \\  |_   _|    | |  | |                   (_)             \r\n"
+				+ "/ /_\\ \\   | |      | |  | |  __ _  _ __  _ __  _   ___   _ __ \r\n"
+				+ "|  _  |   | |      | |/\\| | / _` || '__|| '__|| | / _ \\ | '__|\r\n"
+				+ "| | | |  _| |_     \\  /\\  /| (_| || |   | |   | || (_) || |   \r\n"
+				+ "\\_| |_/  \\___/      \\/  \\/  \\__,_||_|   |_|   |_| \\___/ |_|   \r\n"
+				+ "                                                              ");
+		System.out.println(" ~ Team Doby ~\n");
+		// 게임 스타트 View 31번
+		
+		System.out.println("옛날 용사로 취업하기 위해 6개월 동안 갖은 고통을 감내한 용사 "+name+".\r\n"
+				+ "그는 마지막 6개월이 되는 날 급작스럽게 용사 수련의 증표인 수료증을 받을 수 없다는 소식을 듣게 되는데...\r\n\n");
+		
+		System.out.println("[???]\r\n"
+				+ "안녕하세요?\n");
+		
+		System.out.println("["+name+"]\r\n"
+				+ "누구세요?\n");
+		System.out.println("[용현(자바요정)]\r\n"
+				+ "저는 자바 요정 용현이라고 해요.\r\n"
+				+ "안타깝게도 닉네임 씨는 용사의 증명인 수료증을 받을 수 없게 되었어요.\n");
+		System.out.println("["+name+"]\r\n"
+				+ "왜죠?? 제가 얼마나 6개월 동안 고생을 해왔는데!\n");
+		System.out.println("[용현(자바요정)]\r\n"
+				+ "사실 용사의 증명을 얻기 위해서는 마지막 관문이 남아 있어요.\r\n"
+				+ "바로 3대 마왕을 물리쳐야 해요.");
+		System.out.println("\r\n"
+				+ "["+name+"]\r\n"
+				+ "마왕이라니 6개월동안 QR만 꾸준히 찍으면 되는 게 아니었나요??\n");
+		System.out.println("[용현(자바요정)]\r\n"
+				+ "안!대요! 진실의 방은 피할 수 있었겠지만 취업은 그냥 되는 게 아니예요!\n");
+		System.out.println("["+name+"]\r\n"
+				+ "손나...!! 바카나...!!");
+		// PrologDAO 16번
+	}
+
+}
